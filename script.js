531475653a009c6d290f3034103547259ef64955
Tommaso Cesaro <to.cesaro@studenti.unina.it>
2024-10-17 14:43:37 +0200

    Version 0.9

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
 const padreDiv = document.getElementById("cell-container");
 
-for (let i = 0; i < 76; i++) {
-  let stringa = String(i);
-}
+const funzioneCreatrice = function () {
+  for (let i = 1; i < 77; i++) {
+    let stringa = String(i);
+    console.log("ciao");
+    let divFiglio = document.createElement("div");
+    divFiglio.id = stringa;
+
+    divFiglio.classList.add("cell");
+
+    divFiglio.innerHTML = `
+        <h2>${stringa}</h2>
+          `;
+    padreDiv.appendChild(divFiglio);
+  }
+};
+
+const funzioneRandomica = function () {
+  return Math.floor(Math.random() * 76) + 1;
+};
+
+const funzioneSelected = function () {
+  for (let i = 1; i < 77; i++) {
+    let casellaSelezionata = document.getElementById(
+      String(funzioneRandomica())
+    );
+    if (casellaSelezionata.style.backgroundColor === "red") {
+    } else {
+      casellaSelezionata.style.backgroundColor = "red";
+      break;
+    }
+  }
+};
+
+const funzioneReset = function () {
+  for (let i = 1; i < 77; i++) {
+    let casellaSelezionata = document.getElementById(String(i));
+    casellaSelezionata.style.backgroundColor = "whitesmoke";
+  }
+};
+
+funzioneCreatrice();
+
+const button = document.getElementById("pulsante");
+
+button.addEventListener("click", funzioneSelected);
+
+const buttonReset = document.getElementById("pulsanteReset");
+
+buttonReset.addEventListener("click", funzioneReset);
